bc7c0a9f1f0251d814d813a743fdce6c1c214b74
Jasper Dahil <dahilj@uni.coventry.ac.uk>
2020-09-03 13:19:32 +0100

    added assets and functions to load assets

--- app.py
+++ app.py
@@ -1,3 +1,39 @@
 import tkinter as tk
 import random
 import time
+from PIL import Image, ImageTk
+
+CLOCK_SPEED = 33 #60Hz - Refresh rate
+
+class Graphics(tk.Canvas):
+    def __init__(self):
+        super().__init__(width=600, height=620, highlightthickness=0)
+
+        self.load_assets()
+        self.create_objects()
+
+        self.after(CLOCK_SPEED, self.perform_actions) # initiate window
+    
+    def load_assets(self):
+        try:
+            self.ball_image = Image.open("./assets/ball.png")
+            self.ball_image = self.ball.resize((50, 50), Image.ANTIALIAS)
+            self.ball = ImageTk.PhotoImage(self.ball_image)
+        except IOError as error:
+            print(error)
+            root.destroy()
+
+    def perform_actions(self):
+        '''Recursive function - Refresh window'''
+        self.after(CLOCK_SPEED, self.perform_actions) # Refresh window
+    
+#create window
+root = tk.Tk()
+root.title("Graphics and Animations")
+root.resizable(False, False)
+
+canvas = Graphics()
+canvas.pack()
+
+#start window
+root.mainloop()
