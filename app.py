e3b1d1ed3b710084f2c454bb1765e0aac8fb3ba8
Jasper Dahil <dahilj@uni.coventry.ac.uk>
2020-09-03 12:40:16 +0100

    initial GUI

---
+++ app.py
@@ -0,0 +1,13 @@
+"# animation" 
+from tkinter import *
+import random
+import time
+
+root = Tk()
+
+HEIGHT = 500
+WIDTH = 500
+
+canvas = Canvas(root, width=WIDTH, height=HEIGHT)
+root.title("Graphics and Animation")
+canvas.pack()
