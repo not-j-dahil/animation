208fcdfdd21a7247d34d9748711d567af00cc383
Jasper Dahil <dahilj@uni.coventry.ac.uk>
2020-09-03 13:25:50 +0100

    loaded objects to canvas done next = placement of ball and animation

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ class Graphics(tk.Canvas):
     def __init__(self):
         super().__init__(width=600, height=620, highlightthickness=0)
 
+        self.ball_position = (0, 0)
+
         self.load_assets()
         self.create_objects()
 
@@ -17,11 +19,15 @@ class Graphics(tk.Canvas):
     def load_assets(self):
         try:
             self.ball_image = Image.open("./assets/ball.png")
-            self.ball_image = self.ball.resize((50, 50), Image.ANTIALIAS)
+            self.ball_image = self.ball_image.resize((50, 50), Image.ANTIALIAS)
             self.ball = ImageTk.PhotoImage(self.ball_image)
         except IOError as error:
             print(error)
             root.destroy()
+    
+    def create_objects(self):
+        self.create_image(*self.ball_position, image=self.ball, tag="ball")
+        self.create_rectangle(7,7,593,613, outline="#999999") # border
 
     def perform_actions(self):
         '''Recursive function - Refresh window'''
